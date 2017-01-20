6801a2ae3efbafcbe42f7177dab4a1674f72e76f
Anil Umachigi <M041859@corpau.wbcau.westpac.com.au>
2017-01-20 16:29:23 +1100

    changes to look and feel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  digitalData = {
+    "siteBrand": "wbc",
+    "siteName": "oneclick",
+    "siteSection": "cao",
+    "siteSubSection": "",
+    "siteEnv": "prod",
+    "siteDomain": "www.westpac.com.au",
+    "siteExperience": "mob",
+    "formName": "Cons CC",
+    "formType": "checkurl",
+    "formIsSTP": "true",
+    "formVariant": "na",
+    "newFormName": "cc",
+    "journeyType": "pub",
+    "accountType": "na",
+    "trackOnce": "true",
+    "pageName": "your-details",
+    "eventKey": "mob:wbc_oregon_app_cc_your-details",
+    "pageStep": "start",
+    "pageType": "application",
+    "productID": [{
+      "prod": "WBC03PL005",
+      "qty": "1",
+      "primaryProd": "true"
+    }
+    ]
+  }
+  dataLayer = JSON.stringify(this.digitalData, null, 4);
+
 }
